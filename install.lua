5eeb85c9fa6d85350301f92c5ad19c4d243185ec
Mason Gulu <mason.gulu@gmail.com>
2024-03-29 12:50:09 -0400

    format / fix installer warnings

--- install.lua
+++ install.lua
@@ -71,19 +71,19 @@ local files = {
 
 local writeFile = true -- For debugging purposes
 
-local w,h = term.getSize()
+local w, h = term.getSize()
 local margin = 2
 
 term.setBackgroundColor(colors.white)
 term.setTextColor(colors.lightGray)
 term.clear()
 
-term.setCursorPos(math.floor(w/2-#("Please wait")/2), math.ceil(h/2))
+term.setCursorPos(math.floor(w / 2 - #("Please wait") / 2), math.ceil(h / 2))
 term.write("Please wait")
 
 local strings = require("cc.strings")
 local bigfont = load(
-    http.get("https://pastebin.com/raw/3LfWxRWh").readAll(),
+    assert(http.get("https://pastebin.com/raw/3LfWxRWh").readAll(), "Failed to download bigfont."),
     "bigfont",
     "bt",
     _ENV
@@ -93,49 +93,49 @@ term.setTextColor(colors.black)
 term.clear()
 
 local function title()
-    bigfont.writeOn(term, 1, "Remos", math.floor(w/2-#("Remos"))-1, 2)
+    bigfont.writeOn(term, 1, "Remos", math.floor(w / 2 - #("Remos")) - 1, 2)
 end
 
 title()
 
 local introduction = strings.wrap(
     "Welcome to Remos, an Android inspired shell for ComputerCraft.\n\nIn order to continue the installation, click on \"Continue\".\n\nHold CTRL+T to cancel.",
-    w-(margin*2)
+    w - (margin * 2)
 )
 
 for i, line in ipairs(introduction) do
-    term.setCursorPos(margin, 5+i)
+    term.setCursorPos(margin, 5 + i)
     term.write(line)
 end
 
 local function button(label)
-    term.setCursorPos(1, h-2)
+    term.setCursorPos(1, h - 2)
     term.write(string.char(0x97))
-    term.write(string.char(0x83):rep(w-2))
+    term.write(string.char(0x83):rep(w - 2))
     term.setBackgroundColor(colors.black)
     term.setTextColor(colors.white)
     term.write(string.char(0x94))
 
     term.setBackgroundColor(colors.white)
     term.setTextColor(colors.black)
-    term.setCursorPos(1, h-1)
+    term.setCursorPos(1, h - 1)
     term.clearLine()
     term.write(string.char(0x95))
     term.setBackgroundColor(colors.black)
     term.setTextColor(colors.white)
-    term.setCursorPos(w, h-1)
+    term.setCursorPos(w, h - 1)
     term.write(string.char(0x95))
 
     term.setCursorPos(1, h)
     term.write(string.char(0x8a))
-    term.write(string.char(0x8f):rep(w-2))
+    term.write(string.char(0x8f):rep(w - 2))
     term.setBackgroundColor(colors.black)
     term.setTextColor(colors.white)
     term.write(string.char(0x85))
 
     term.setBackgroundColor(colors.white)
     term.setTextColor(colors.black)
-    term.setCursorPos(math.ceil(w/2-#label/2), h-1)
+    term.setCursorPos(math.ceil(w / 2 - #label / 2), h - 1)
     term.write(label)
 end
 
@@ -144,14 +144,14 @@ button("Continue")
 while true do
     local event, button, _, y = os.pullEventRaw()
     if event == "mouse_click" then
-        if y >= h-2 and button == 1 then
+        if y >= h - 2 and button == 1 then
             break
         end
     elseif event == "terminate" then
         term.setBackgroundColor(colors.black)
         term.setTextColor(colors.white)
         term.clear()
-        term.setCursorPos(1,1)
+        term.setCursorPos(1, 1)
         return
     end
 end
@@ -163,11 +163,11 @@ term.clear()
 
 local info = strings.wrap(
     "Don't turn off the device, this might take a while",
-    w-(margin*2)
+    w - (margin * 2)
 )
 
 for i, line in ipairs(info) do
-    term.setCursorPos(math.floor(w/2-#line/2)+margin-1, h-#info+i-1)
+    term.setCursorPos(math.floor(w / 2 - #line / 2) + margin - 1, h - #info + i - 1)
     term.write(line)
 end
 
@@ -178,14 +178,14 @@ title()
 local function part(progress)
     return strings.wrap(
         ("Now installing\n%d%% complete"):format(progress),
-        w-(margin*2)
+        w - (margin * 2)
     )
 end
 
 local animation = "\133\131\138\151\143\148"
 local inverse = "\151\143"
 
-local cur, total = 0,100
+local cur, total = 0, 100
 local successfull, reason = false, "Unknown exception"
 parallel.waitForAny(
     function()
@@ -201,19 +201,19 @@ parallel.waitForAny(
                 term.setTextColor(colors.black)
             end
 
-            bigfont.writeOn(term, 1, state, math.floor(w/2), math.floor(h/2)-2)
-            
+            bigfont.writeOn(term, 1, state, math.floor(w / 2), math.floor(h / 2) - 2)
+
             term.setBackgroundColor(colors.white)
             term.setTextColor(colors.black)
-        
-            step = step +1
+
+            step = step + 1
             if step > #animation then
                 step = 1
             end
 
-            local text = part(math.floor(100/total*cur))
+            local text = part(math.floor(100 / total * cur))
             for i, line in ipairs(text) do
-                term.setCursorPos(math.floor(w/2-#line/2)+margin-1, math.floor(h/2)+i+1)
+                term.setCursorPos(math.floor(w / 2 - #line / 2) + margin - 1, math.floor(h / 2) + i + 1)
                 term.write(line)
             end
 
@@ -304,21 +304,21 @@ term.clear()
 
 title()
 
-local final = ""
+local final = { "" }
 if successfull then
     final = strings.wrap(
         "Installation complete! \2\n\nHold CTRL+T if wishing to make manual changes before applying the changes.",
-        w-(margin*2)
+        w - (margin * 2)
     )
 else
     final = strings.wrap(
-        ":(\nA fatal error has occurred. The installation has been cancelled.\n\nReason:\n\t"..reason,
-        w-(margin*2)
+        ":(\nA fatal error has occurred. The installation has been cancelled.\n\nReason:\n\t" .. reason,
+        w - (margin * 2)
     )
 end
 
 for i, line in ipairs(final) do
-    term.setCursorPos(margin, 5+i)
+    term.setCursorPos(margin, 5 + i)
     term.write(line)
 end
 
@@ -327,14 +327,14 @@ button("Restart")
 while true do
     local event, button, _, y = os.pullEventRaw()
     if event == "mouse_click" then
-        if y >= h-2 and button == 1 then
+        if y >= h - 2 and button == 1 then
             break
         end
     elseif event == "terminate" then
         term.setBackgroundColor(colors.black)
         term.setTextColor(colors.white)
         term.clear()
-        term.setCursorPos(1,1)
+        term.setCursorPos(1, 1)
         return
     end
 end
